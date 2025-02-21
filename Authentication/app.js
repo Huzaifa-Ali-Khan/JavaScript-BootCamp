db3808d69c03681c6f592a641637b27e124d3a0c
Huzaifa-Ali-Khan <huzaifalikhan48@gmail.com>
2025-02-21 22:54:23 +0500

    Add authentication functionality with signup and login forms

---
+++ Authentication/app.js
@@ -0,0 +1,57 @@
+let Button = document.getElementById("button");
+let Email = document.getElementById("email");
+let Password = document.getElementById("password");
+let Name = document.getElementById("name");
+
+const submitHandler = async () => {
+  if (!Name.value || !Email.value || !Password.value) {
+    alert("Please fill in all fields");
+    return;
+  }
+  try {
+    const response = await fetch(
+      "http://192.168.100.33:8082/keeps/user/signUp",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: Email.value,
+          password: Password.value,
+          name: Name.value,
+        }),
+      }
+    );
+    const data = await response.json();
+    alert(data.message);
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const loginHandler = async () => {
+  try {
+    const response = await fetch(
+      "http://192.168.100.33:8082/keeps/user/signIn",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: Email.value,
+          password: Password.value,
+        }),
+      }
+    );
+    const data = await response.json();
+    alert(data.message);
+    console.log(data.token);
+    localStorage.setItem("userKeepToken", data.token);
+    console.log(data);
+  } catch (error) {
+    alert(error.message);
+  }
+};
